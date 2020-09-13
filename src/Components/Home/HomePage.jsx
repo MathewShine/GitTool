0f9f3616e56d0f0b7698599d18fbcf2b56b53a7a
MathewJR <shine@dobbyads.com>
2020-09-14 00:35:05 +0530

    Basic Home UI Done

---
+++ src/Components/Home/HomePage.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import clsx from 'clsx';
+import { useTheme } from '@material-ui/core/styles';
+import {
+    Drawer,
+    AppBar,
+    Toolbar, 
+    List,
+    CssBaseline,
+    Typography ,
+    Divider,
+    IconButton
+
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import { HomePageStyles } from './HomePageStyles';
+
+export default function MiniDrawer() {
+  const classes =  HomePageStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Git Tool
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Search User', 'Search Repository'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon className={classes.ListItemIcon}>{index % 2 === 0 ? <AccountCircleIcon /> : <FileCopyIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+         
+        </Typography>
+        <Typography paragraph>
+        
+        </Typography>
+      </main>
+    </div>
+  );
+}
